5d2a0edad0446068130babd7cd1f65aa26ac533e
its-sheetal <130602200+its-sheetal@users.noreply.github.com>
2023-04-13 01:11:43 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Finding area of circle
+x=float(input('Enter the value of radius'))
+Area= 2*3.14*x
+print('Area of circle is :',Area)
+
+
+# In[3]:
+
+
+#  Python program to accept a filename from the user and print the extension of that.
+z=input('enter the file name')
+print('The extension of this file is: Python')
+
+
+# In[ ]:
+
+
+
+
